f47a0e5aeb38fbb041e4475e945ab8a736cebefd
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-03-09 22:24:38 +0000

    Create decisions.py

---
+++ Week-4/decisions.py
@@ -0,0 +1,14 @@
+# use statements to create a program to manipulate integers
+# if a positive even integer is entered / by 2
+# if a positive odd integer is entered x by 3 and + 1
+# break when you get to 1
+
+
+f = input ("a positive integer (1 to quit)")
+f = int(f)
+print (f)
+while (f > 1): 
+    if f % 2 == 0: f = (f / 2)
+
+    else: f = (f*3+1)
+    print (f)
