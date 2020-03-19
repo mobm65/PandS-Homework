15299256b7d27758e20dc320c1f95712b310693a
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-03-19 13:02:05 +0000

    Create weekendyeaweekdaynay.py
    Code to give a negative response to input of midweek days and positive  response to weekend day.

---
+++ Week-4/week-5/weekendyeaweekdaynay.py
@@ -0,0 +1,14 @@
+# Creating a program to respond positively to
+# weekend days and negatively to week days.
+# Weekday output - Yes, unfortunately today is a weekday.
+# Weekend output - It is the weekend, yay!
+
+W = [ "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday", "Sunday"]
+
+# Input one element of W, day
+input("day")
+while input 
+    if input = W[0:4]
+    print ("Yes, unfortunately today is a weekday")
+    else: print ("It is the weekend, yay!")
