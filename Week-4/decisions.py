06f029159ff026413b269b2d091f967c19b9878d
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-03-29 16:54:57 +0100

    Final commit
    I became confused and attempted this again in a very roundabout way.

--- Week-4/decisions.py
+++ Week-4/decisions.py
@@ -1,27 +1,15 @@
-    # use statements to create a program to manipulate integers
+# use statements to create a program to manipulate integers
 # if a positive even integer is entered / by 2
 # if a positive odd integer is entered x by 3 and + 1
 # break when you get to 1
+# I had completed this weeks ago using notes and Whirlwind Tour of Python
 
 
-f = input("Input a positive integer (1 to quit): ")
-  
 
-for f in range(1, f):
-    if f % 2 == 0:
-        continue 
-    print(f, end= "")
-
-    
-for f in range(1, f):
-    if f % 2 != 0:
-        continue
-print(f * 3 + 1)
-
-
-    #if (f % 2 == 0):
- #   print(f / 2)
-#else: 
- #   print(f * 3 + 1)
-
-#print(f)
+f = input ("a positive integer (1 to quit): ")
+f = int(f)
+print (f)
+while (f > 1): 
+    if f % 2 == 0: f = (f / 2)
+    else: f = (f*3+1)
+    print (f)
