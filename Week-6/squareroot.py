f9d206d682a11cfce9a34cac428e141e130be2e5
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-03-29 19:29:47 +0100

    first commit
    importing math and sqrt

--- Week-6/squareroot.py
+++ Week-6/squareroot.py
@@ -1,3 +1,15 @@
 # A function called sqrt which takes a 
 # floating-point number as imput and outputs
 # an approximation of its square root.  
+# Refererence RealPython
+
+
+
+
+f = input("input float: ")
+#f = float(f)
+
+import math 
+ 
+math.sqrt(f)
+print(f)
